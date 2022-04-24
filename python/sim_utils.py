947486d2e95648047a33d7221bdef817f6617196
Elizabeth Champion <elizabethchampion20@gmail.com>
2022-04-23 21:58:03 -0400

    got two-qubit simulation working, with entangling

--- python/sim_utils.py
+++ python/sim_utils.py
@@ -5,24 +5,30 @@ import qutip as qt
 ### Generates the Kerr-nonlinear part of the Hamiltonian with Kerr nonlinearity
 ### K, a Hilbert space of dimension N *for each resonator*, and nres resonators.
 ###
-def H_kerr(K, N, nres=1):
+def H_kerr(K, N, nres=1, res_index=0):
     H = -K * qt.create(N)**2 * qt.destroy(N)**2
-    return qt.tensor([H] * nres)
+    H_list = [qt.identity(N)] * nres
+    H_list[res_index] = H
+    return qt.tensor(H_list)
 
 ###
 ### The two-photon drive for a single resonator
 ###
-def H_2ph(e_2, N, strength_factor=(lambda t, _: 1.), nres=1, res_index=0):
+def H_2ph(e_2, N, tau=None, t0=None, direction=1, nres=1, res_index=0):
     H = e_2 * qt.create(N)**2 + np.conj(e_2) * qt.destroy(N)**2
     H_list = [qt.identity(N)] * nres
     H_list[res_index] = H
+    if tau is not None and t0 is not None:
+        strength_factor = "0.5 * tanh(4. * ({0} * t - {1}) / {2} - 2.) + 0.5".format(direction, t0, tau)
+    else:
+        strength_factor = "1."
     return [qt.tensor(H_list), strength_factor]
 
 ###
 ### The four-photon drive for a single resonator
 ###
-def H_4ph(e_2, N, strength_factor=(lambda t, _: 1.), nres=1, res_index=0):
-    H = e_2 * qt.create(N)**4 + np.conj(e_2) * qt.destroy(N)**4
+def H_4ph(e_4, N, strength_factor=(lambda t, _: 1.), nres=1, res_index=0):
+    H = e_4 * qt.create(N)**4 + np.conj(e_4) * qt.destroy(N)**4
     H_list = [qt.identity(N)] * nres
     H_list[res_index] = H
     return [qt.tensor(H_list), strength_factor]
@@ -31,14 +37,18 @@ def H_4ph(e_2, N, strength_factor=(lambda t, _: 1.), nres=1, res_index=0):
 ### The coupling Hamiltonian. Note that the number of resonators is not an
 ### argument because it only makes sense to do this with two resonators.
 ###
-def H_c(g, N, strength_factor=(lambda t, _: 1.)):
-    return [g * (tensor(qt.create(N), qt.destroy(N)) + qt.tensor(qt.destroy(N), qt.create(N))), strength_factor]
+def H_c(g_12, N, tau=None, t0=None, direction=1):
+    if tau is not None and t0 is not None:
+        strength_factor = "0.5 * tanh(4. * ({0} * t - {1}) / {2} - 2.) + 0.5".format(direction, t0, tau)
+    else:
+        strength_factor = "1."
+    return [g_12 * (qt.tensor(qt.create(N), qt.destroy(N)) + qt.tensor(qt.destroy(N), qt.create(N))), strength_factor]
 
 ###
 ### A function to generate our basis (cat) states for us
 ###
-def cat_states(K, e_2, N):
-    alpha = np.sqrt(e_2 / K)
+def cat_states(K, e, N):
+    alpha = np.sqrt(e / K)
     psi_0 = (qt.coherent(N, alpha) + qt.coherent(N, -alpha)) / np.sqrt(2. * (1 + np.exp(-2. * np.abs(alpha)**2)))
     psi_1 = (qt.coherent(N, alpha) - qt.coherent(N, -alpha)) / np.sqrt(2. * (1 - np.exp(-2. * np.abs(alpha)**2)))
     return psi_0, psi_1
@@ -49,7 +59,7 @@ def cat_states(K, e_2, N):
 ###
 def basis(K, e_2, N):
     psi_0, psi_1 = cat_states(K, e_2, N)
-    return qt.tensor(psi_0, psi_0), qt.tensory(psi_0, psi_1), qt.tensor(psi_1, psi_0), qt.tensor(psi_1, psi_1)
+    return [qt.tensor(psi_0, psi_0), qt.tensor(psi_0, psi_1), qt.tensor(psi_1, psi_0), qt.tensor(psi_1, psi_1)]
 
 ###
 ### Compute the probability that one resonator is in the given state
